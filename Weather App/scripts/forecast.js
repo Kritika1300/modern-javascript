737ef50468a5d0d229117faef343c72d06e19c62
Kritika <kaur.kritika.kritika@gmail.com>
2021-02-21 16:54:34 +0530

    getCity API

---
+++ Weather App/scripts/forecast.js
@@ -0,0 +1,11 @@
+const key = "Lgv6KAAwYhtu6CrUclX2uSHhenZMfdth";
+
+//for city info
+ const getCity = async (city) =>{
+ const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+ const query = `?apikey=${key}&q=${city}`;
+ const response = await fetch(base + query);
+ const data = await response.json();
+ return data;
+};
+getCity('chandigarh').then(data => console.log(data)).catch(err => console.log(err));
