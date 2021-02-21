44bb025f70fdee0328111fe4b4cfaeb1f4100f24
Kritika <kaur.kritika.kritika@gmail.com>
2021-02-21 17:54:19 +0530

    getWeather API

--- Weather App/scripts/forecast.js
+++ Weather App/scripts/forecast.js
@@ -1,4 +1,12 @@
-const key = "Lgv6KAAwYhtu6CrUclX2uSHhenZMfdth";
+ const key = "Lgv6KAAwYhtu6CrUclX2uSHhenZMfdth";
+//for weather info
+ const getWeather = async (id) => {
+ const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+ const query = `${id}?apikey=${key}`;
+ const response = await fetch(base + query);
+ const data = await response.json();
+ return data[0];
+};
 
 //for city info
  const getCity = async (city) =>{
@@ -6,6 +14,9 @@ const key = "Lgv6KAAwYhtu6CrUclX2uSHhenZMfdth";
  const query = `?apikey=${key}&q=${city}`;
  const response = await fetch(base + query);
  const data = await response.json();
- return data;
+ return data[0];
 };
-getCity('chandigarh').then(data => console.log(data)).catch(err => console.log(err));
+getCity('chandigarh').then(data => 
+   {return getWeather(data.Key);}).then(data =>{
+       console.log(data);
+   }).catch(err => console.log(err));
