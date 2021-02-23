d9bdfbedf6734548165f5f1b34599a36f6dbb2e6
Kritika <kaur.kritika.kritika@gmail.com>
2021-02-23 23:49:04 +0530

    Adding collection data to the app

--- Firebase Practice/sandbox.js
+++ Firebase Practice/sandbox.js
@@ -1,3 +1,13 @@
+const list = document.querySelector('ul');
+const addRecipe = (recipe) => {
+       const time = recipe.created_at.toDate();
+       let html = `<div> <li>${recipe.title}</div>
+                  <div> ${time}</li> </div>`
+       list.innerHTML += html;
+};
 db.collection('recipes').get().then((snapshot) =>{
-    console.log(snapshot.docs[0].data());
+    snapshot.docs.forEach(doc =>{
+        addRecipe(doc.data());
+        //console.log(doc.data());
+    });
     }).catch(err => {console.log(err)});
